718a40297d04a9de668c88054378bfe5ecc15864
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-07-07 17:09:07 +0700

    manage hompage

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- async redirects () {
-  return [
-    {
-      source: '/',
-      destination : '/home',
-      permanent: true
-    }
-  ]
- }
+
 };
 
 export default nextConfig;
